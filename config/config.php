726793898ef134e0f923ed357cce461d71dface7
ALTELMA <nrohtgnohp@gmail.com>
2021-08-20 14:51:24 +0700

    Update Server exception

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'auth'          => env('AUTH_SERVICE_URL'),
-    'location'      => env('LOCATION_SERVICE_URL'),
-    'thor'          => env('THOR_SERVICE_URL'),
-    'oauth'         => env('OAUTH_SERVICE_URL'),
-    'url-shortener' => env('URL_SHORTENER_SERVICE_URL'),
-    'user'          => env('USER_SERVICE_URL'),
-    'user-extended' => env('USER_EXTENDED_SERVICE_URL'),
+    'auth'                => env('AUTH_SERVICE_URL'),
+    'backoffice'          => env('BACKOFFICE_SERVICE_URL'),
+    'location'            => env('LOCATION_SERVICE_URL'),
+    'thor'                => env('THOR_SERVICE_URL'),
+    'oauth'               => env('OAUTH_SERVICE_URL'),
+    'url-shortener'       => env('URL_SHORTENER_SERVICE_URL'),
+    'user'                => env('USER_SERVICE_URL'),
+    'user-extended'       => env('USER_EXTENDED_SERVICE_URL'),
 ];
--- src/Exceptions/SsoServerErrorException.php
+++ src/Exceptions/SsoServerErrorException.php
@@ -8,21 +8,35 @@ use GuzzleHttp\Exception\RequestException;
 class SsoServerErrorException extends ApplicationHttpException
 {
     const SSO_ERROR_CODE = [
-        'SSO_SERVICE_UNKNOWN_ERROR'               => 'sso_service_unknown_error',
-        'SSO_AUTH_SERVICE_SERVER_ERROR'           => 'sso_auth_service_server_error',
-        'SSO_AUTH_SERVICE_UNAVAILABLE'            => 'sso_auth_service_unavailable',
-        'SSO_BACKOFFICE_SERVICE_UNAVAILABLE'      => 'sso_backoffice_service_unavailable',
-        'SSO_LOCATION_SERVICE_SERVER_ERROR'       => 'sso_location_service_server_error',
-        'SSO_LOCATION_SERVICE_UNAVAILABLE'        => 'sso_location_service_unavailable',
+        'SSO_SERVICE_UNKNOWN_ERROR'                    => 'sso_service_unknown_error',
+        'SSO_AUTH_SERVICE_SERVER_ERROR'                => 'sso_auth_service_server_error',
+        'SSO_AUTH_SERVICE_UNAVAILABLE'                 => 'sso_auth_service_unavailable',
+        'SSO_BACKOFFICE_SERVICE_ERROR'                 => 'sso_backoffice_service_server_error',
+        'SSO_BACKOFFICE_SERVICE_UNAVAILABLE'           => 'sso_backoffice_service_unavailable',
+        'SSO_LOCATION_SERVICE_SERVER_ERROR'            => 'sso_location_service_server_error',
+        'SSO_LOCATION_SERVICE_UNAVAILABLE'             => 'sso_location_service_unavailable',
+        'SSO_THOR_SERVICE_SERVER_ERROR'                => 'sso_thor_service_server_error',
+        'SSO_THOR_SERVICE_UNAVAILABLE'                 => 'sso_thor_service_unavailable',
+        'SSO_URL_SHORTENER_SERVICE_SERVER_ERROR'       => 'sso_url_shortener_service_server_error',
+        'SSO_URL_SHORTENER_SERVICE_UNAVAILABLE'        => 'sso_url_shortener_service_unavailable',
+        'SSO_USER_SERVICE_SERVER_ERROR'                => 'sso_user_service_server_error',
+        'SSO_USER_SERVICE_UNAVAILABLE'                 => 'sso_user_service_unavailable',
     ];
 
     protected static array $ssoErrorHttpStatusCode = [
-        'sso_service_unknown_error'               => 500,
-        'sso_auth_service_server_error'           => 500,
-        'sso_auth_service_unavailable'            => 503,
-        'sso_backoffice_service_unavailable'      => 503,
-        'sso_location_service_server_error'       => 500,
-        'sso_location_service_unavailable'        => 503,
+        'sso_service_unknown_error'                     => 500,
+        'sso_auth_service_server_error'                 => 500,
+        'sso_auth_service_unavailable'                  => 503,
+        'sso_backoffice_service_server_error'           => 500,
+        'sso_backoffice_service_unavailable'            => 503,
+        'sso_location_service_server_error'             => 500,
+        'sso_location_service_unavailable'              => 503,
+        'sso_thor_service_server_error'                 => 500,
+        'sso_thor_service_unavailable'                  => 503,
+        'sso_url_shortener_service_server_error'        => 500,
+        'sso_url_shortener_service_unavailable'         => 503,
+        'sso_user_service_server_error'                 => 500,
+        'sso_user_service_unavailable'                  => 503,
     ];
 
     public function __construct(
